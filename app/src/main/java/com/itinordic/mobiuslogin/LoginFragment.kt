76047e73edfa699322c3e01f29682a7e3959007d
Brisis <38928449+Brisis@users.noreply.github.com>
2022-03-28 20:24:31 +0200

    Clean Up (Login Fragment)

--- app/src/main/java/com/itinordic/mobiuslogin/LoginFragment.kt
+++ app/src/main/java/com/itinordic/mobiuslogin/LoginFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_login.*
@@ -35,28 +36,32 @@ class LoginFragment : Fragment(), View.OnClickListener {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
-    val loginRepository = LoginService()
+    private val loginRepository = LoginService()
 
     override fun onClick(v: View?) {
         when(v!!.id){
             R.id.sign_in_button -> {
-                if(!TextUtils.isEmpty(username_input.text.toString()) && !TextUtils.isEmpty(password_input.text.toString())){
+                val username = username_input.text.toString()
+                val password = password_input.text.toString()
 
-                    loginRepository.login(username_input.text.toString(),password_input.text.toString())
-                    //
-//                    var bundle = bundleOf(
-//                        "user" to user, to be created
-//                    )
-//                    navController!!.navigate(
-//                        R.id.action_loginFragment2_to_profileFragment2,
-//
-//                    )
+                if(username.isNotEmpty() && password.isNotEmpty()){
+
+                    val doLogin = loginRepository.login(username,password)
+                    if (doLogin){
+                        val bundle = bundleOf(
+                            "username" to username,
+                        )
+                        navController!!.navigate(
+                            R.id.action_loginFragment2_to_profileFragment2,
+                            bundle
+                        )
+                    }
+
                 }
                 else{
                     Toast.makeText(activity, "Enter details to login", Toast.LENGTH_SHORT).show()
                 }
             }
-            R.id.sign_in_button -> navController!!.navigate(R.id.action_loginFragment2_to_profileFragment2)
         }
     }
 
